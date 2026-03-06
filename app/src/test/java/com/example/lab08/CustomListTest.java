8aa83288795ac9f1618d2aeb05143e74d6db7e37
Karina-Zhangg <xz30@ualberta.ca>
2026-03-05 17:39:22 -0700

    running a failed test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -7,7 +7,7 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-        // This will fail initially because hasCity() doesn't
+        // This will fail initially because hasCity() doesn't.
         exist
         assertTrue(list.hasCity(calgary));
     }
